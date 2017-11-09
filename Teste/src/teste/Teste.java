c1bf377b9c65fcffae1fdeba8c7126791e4dd0bb
Adam <Adam@PC-Adam>
2017-11-09 14:18:23 -0400

    Modificação 1

--- Teste/src/teste/Teste.java
+++ Teste/src/teste/Teste.java
@@ -4,6 +4,9 @@ public class Teste {
 
     public static void main(String[] args) {
         System.out.println("xD");
+        System.out.println("xD");
+        System.out.println("xD");
+        System.out.println("xD");
     }
 
 }
